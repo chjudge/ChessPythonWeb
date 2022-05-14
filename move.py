ca6e1d260889928f9e9874a140c294e7fa9dbf4f
Peter Judge <pjudge2002@gmail.com>
2022-05-14 14:40:19 -0400

    create and intializing move class

---
+++ move.py
@@ -0,0 +1,10 @@
+
+class move():
+    def __init__(self):
+        self.startPosR = None
+        self.startPosC = None
+        self.endPosR = None
+        self.endPosC = None
+    
+
+    print("HI")
