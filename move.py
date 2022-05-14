d2e0469004c326c4eee599bf4298f2628cd8647b
Peter Judge <pjudge2002@gmail.com>
2022-05-14 14:48:23 -0400

    made some changes and finished the move class

--- move.py
+++ move.py
@@ -1,10 +1,28 @@
 
-class move():
+class Move():
     def __init__(self):
         self.startPosR = None
         self.startPosC = None
         self.endPosR = None
         self.endPosC = None
+
+# Setter Method
+    def setMove(self, sPR, sPC, ePR, ePC):
+        self.startPosR = sPR
+        self.startPosC = sPC
+        self.endPosR = ePR
+        self.endPosC = ePC
+
+# Getter Methods
+    def getStartR(self):
+        return self.startPosR
+    def getStartC(self):
+        return self.startPosC
+    def getEndR(self):
+        return self.endPosR
+    def getEndC(self):
+        return self.endPosC
+
     
 
     print("HI")
