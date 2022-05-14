6c10afe0291c31c9607bcbf4c5c5743b1845a96d
Peter Judge <pjudge2002@gmail.com>
2022-05-14 15:58:42 -0400

    stub method for pieces

--- pieces.py
+++ pieces.py
@@ -1,1 +1,25 @@
+from abc import ABC
+class Pieces(ABC):
+    # print("Hi")
+    def __init__(self, xPos, yPos, color):
+        self.xPos = xPos
+        self.yPos = yPos
+        self.color = color
+        self.hasMoved = False
+    # abstract function for the movment o each type of piece
+    def canMove(self):
+        pass
+    # checks to see if piece can move
+    # takes in argument move (which has start and end position)
+    def pieceInWay(self, move):
+        return False
 
+    # getter methods
+    def getX(self):
+        return self.xPos
+    def getY(self):
+        return self.yPos
+    def getColor(self):
+        return self.color
+    def hasMoved(self):
+        return self.hasMoved
