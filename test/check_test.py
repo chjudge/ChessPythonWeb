90b870760af0b387c3b051b60a10e133c5f0febc
Peter Judge <pjudge2002@gmail.com>
2022-08-01 13:23:47 -0400

    few changes made

--- test/check_test.py
+++ test/check_test.py
@@ -43,3 +43,22 @@ def testKnightCheck():
     board.move_piece(move, True)
     print(board)
     assert board.get_piece(0, 4).king_check(0, 4, board)
+
+def testQueenCheck():
+    board = Board()
+    move = Move()
+    move.define_move(6, 4, 5, 4)
+    board.move_piece(move, True)
+    move.define_move(7, 3, 4, 6)
+    board.move_piece(move, True)
+    move.define_move(7, 5, 3, 1)
+    board.move_piece(move, True)
+    move.define_move(4, 6, 1, 3)
+    board.move_piece(move, True)
+    print(board)
+    assert board.get_piece(0, 4).king_check(0, 4, board)
+    move.define_move(0, 3, 1, 3)
+    board.move_piece(move, False)
+    print(board)
+    assert board.get_piece(0, 4).king_check(0, 4, board) == False
+
