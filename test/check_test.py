44e783a21c45c16c4f5456f83717895994498c0a
Peter Judge <pjudge2002@gmail.com>
2022-07-31 21:23:40 -0400

    check test

--- test/check_test.py
+++ test/check_test.py
@@ -1,5 +1,20 @@
 from board import Board
 from move import Move
+from pieces.empty import Empty
+from pieces.rook import Rook
 from pieces.king import King
+from pieces.pawn import Pawn
+from pieces.queen import Queen
 
+def testBishopCheck():
+    board = Board()
+    move = Move()
+    move.define_move(1, 5, 3, 5)
+    board.move_piece(move, True)
+    # move.define_move(7, 4, 6, 4)
+    # board.move_piece(move, False)
+    # move.define_move(1, 6, 5, 2)
+    # board.move_piece(move, True)
+    print(board)
+    assert board.get_piece(5, 8).king_check(5, 8, board)
 
